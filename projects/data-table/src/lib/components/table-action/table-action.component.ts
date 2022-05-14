a7cc3073666c3b0a0aca234a3d6c1b367d6ec3f3
Ayoub Alfurjani <a.alfurjani@muljin.com>
2022-05-14 09:31:18 +0200

    ported the action component

--- projects/data-table/src/lib/components/table-action/table-action.component.ts
+++ projects/data-table/src/lib/components/table-action/table-action.component.ts
@@ -1,12 +1,12 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { TableAction } from '../../types/table-action';
 
 @Component({
   selector: 'table-action',
   templateUrl: './table-action.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TableActionComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class TableActionComponent {
+  @Input() action: TableAction | null = null;
+  @Input() row: any = null;
 }
